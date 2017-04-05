b3f695ab80faf0ba8e2ec8c15349233206b0cd44
Le Ngoc Duy <askbills@gmail.com>
2017-04-05 13:18:17 +0700

    add Procfile to declare what commands are run by your application’s dynos on the Heroku platform

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://mongo/nodejwt'
+    'database': 'mongodb://admin:askbills@ds151820.mlab.com:51820/heroku_21tbbq94'
 };
